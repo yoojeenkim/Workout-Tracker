9f87cc86e3ef137b27c3d83ab2ebd97c5901274d
daniel <yoojeenkim@gmail.com>
2021-11-08 14:14:07 -0500

    added routes for static pages

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = require('../models');
+const db = require('../models/workout');
 
 mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 app.use(require("./routes/api.js"));
+app.use(require("./routes/index.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`)
