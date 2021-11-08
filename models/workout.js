5faaef40b478594e14779506ee5a1c313aab81c4
daniel <yoojeenkim@gmail.com>
2021-11-08 14:17:47 -0500

    moved exercises into workout schema

--- models/workout.js
+++ models/workout.js
@@ -7,9 +7,23 @@ const workoutSchema = new Schema({
         type: Date
     },
     exercises: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Exercise"
+        type: {
+            type: String
+        },
+        name: {
+            type: String
+        },
+        duration: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
         }
     ]
 });
